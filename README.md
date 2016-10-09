add4f404cca333df280357844080a9208d93545f
free0xff <free.0xff@outlook.com>
2016-10-09 17:47:20 +0800

    1. update some code blocks and fix several bugs

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # wait-for-it
 python wait-for-it for docker container 
+
+## Usage
+
+```
+wait-for-it.py host:port [-s] [-t timeout]
+-h HOST | --host=HOST       Host or IP under test
+-p PORT | --port=PORT       TCP port under test
+                            Alternatively, you specify the host and port as host:port
+-q | --quiet                Don't output any status messages
+-t TIMEOUT | --timeout=TIMEOUT
+                            Timeout in seconds, zero for no timeout
+```
+
+## Examples
+
+For example, let's test to see if we can access port 80 on www.baidu.com, and if it is available, the script will print the successful message `wait-for-it.py: www.baidu.com:80 is available after n seconds`,where the n is the waiting seconds,otherwise it will output the timeout message `wait-for-it.py: timeout occurred after waiting n seconds for wait-for-it.py`,where the n equals timeout seconds,the default value is 15 seconds, you can modify it's value by setting `-t` or `--timeout` options.
+
+```
+$ python wait-for-it.py www.baidu.com:80
+wait-for-it.sh: waiting 15 seconds for www.baidu.com:80
+wait-for-it.sh: www.baidu.com:80 is available after 0 seconds
+```
+
+You can set your own timeout with the `-t` or `--timeout=` option.  Setting the timeout value to 0 will disable the timeout:
+
+```
+$ python wait-for-it.py -t 0 www.baidu.com:80
+wait-for-it.py: waiting for www.google.com:80 without a timeout
+wait-for-it.py: www.baidu.com:80 is available after 0 seconds
+```
+
+The subcommand will be executed regardless if the service is up or not.  If you wish to execute the subcommand only if the service is up, add the `--strict` argument. In this example, we will test port 81 on www.google.com which will fail:
+
+```
+$ ./wait-for-it.sh www.google.com:81 --timeout=1 --strict -- echo "google is up"
+wait-for-it.sh: waiting 1 seconds for www.google.com:81
+wait-for-it.sh: timeout occurred after waiting 1 seconds for www.google.com:81
+wait-for-it.sh: strict mode, refusing to execute subprocess
+```
+
+If you don't want to execute a subcommand, leave off the `--` argument.  This way, you can test the exit condition of `wait-for-it.sh` in your own scripts, and determine how to proceed:
+
+```
+$ ./wait-for-it.sh www.google.com:80
+wait-for-it.sh: waiting 15 seconds for www.google.com:80
+wait-for-it.sh: www.google.com:80 is available after 0 seconds
+$ echo $?
+0
+$ ./wait-for-it.sh www.google.com:81
+wait-for-it.sh: waiting 15 seconds for www.google.com:81
+wait-for-it.sh: timeout occurred after waiting 15 seconds for www.google.com:81
+$ echo $?
+124
+```
--- wait-for-it.py
+++ wait-for-it.py
@@ -1,118 +1,77 @@
-#-*- encoding=utf-8 -*-
-#!/bin/usr/env python3
+#!/bin/usr/env python
+
+from optparse import OptionParser
 import socket
 import time
-import getopt
 import sys
+class OptionException(Exception):
+    def __init__(self, value):
+        self.value = value
 
-# global
-quiet = 0
+class wait_for_app:
+    def log(self, loginfo):
+        if self.options.quiet is not False:
+            print(loginfo)
 
-def usage():
-	usage_info = '''
-Usage:
-    %s host:port [-t timeout] [-- command args]
-    -h HOST | --host=HOST       Host or IP under test
-    -p PORT | --port=PORT       TCP port under test
-                                Alternatively, you specify the host and port as host:port
-    -q | --quiet                Don't output any status messages
-    -t TIMEOUT | --timeout=TIMEOUT
-                                Timeout in seconds, zero for no timeout
-    --help                      Print usage
-''' %(sys.argv[0])
-	print(usage_info)
+    def build_log(self, type, app, time=0):
+        # 1=enable_timeout,2=disable_timeout,3=success_msg,4=unavailable,5=timeout_msg
+        loginfo = {
+             1:"%s: waiting %d seconds for %s" %(sys.argv[0],time,app),
+             2:"%s: waiting for %s without a timeout" %(sys.argv[0],app),
+             3:"%s: %s is available after %d seconds" %(sys.argv[0],app,time),
+             4:"%s: %s is unavailable" %(sys.argv[0],app),
+             5:"%s: timeout occurred after waiting %d seconds for %s" %(sys.argv[0],time,app),
+        }.get(type)
+        return loginfo
 
-def log(loginfo):
-	global quiet
-	if quiet == 0:
-		print(loginfo)
-
-def build_log(type,app,time=0):
-	# 1=enable_timeout,2=disable_timeout,3=success_msg,4=timeout_msg
-	loginfo = {
-		 1:"%s: waiting %d seconds for %s" %(sys.argv[0],time,app),
-		 2:"%s: waiting for %s without a timeout" %(sys.argv[0],app),
-		 3:"%s: %s is available after %d seconds" %(sys.argv[0],app,time),
-		 4:"%s: timeout occurred after waiting %d seconds for %s" %(sys.argv[0],time,app),
-	}.get(type)
-	return loginfo
-	
-def wait_for(host, port, timeout):
-	# output some logs
-	disable_timeout = False
-	if timeout == 0: enable_timeout = False
-	app = "%s:%d" %(host,port)
-	loginfo = build_log(1,app,timeout)
-	if disable_timeout: loginfo = build_log(2,app)
-	log(loginfo)
-	
-	# get start time, then attempt to connect,
-	# if succeed,then timeout_flag=0,otherwise timeout,timeout_flag=1
-	timeout_flag = 0
-	start_ts = time.time()
-	sk = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	while True:
-		try:
-			sk.connect((host, port))
-			break
-		except:
-			if disable_timeout:	continue
-			time.sleep(1)
-			timeout = timeout-1
-			if timeout == 0:
-				timeout_flag = 1
-				break
-	#succeed or timeout log
-	end_ts = time.time()
-	diff_ts = int(end_ts - start_ts)
-	if timeout_flag == 0:
-		loginfo = build_log(3,app,diff_ts)
-	else:
-		loginfo = build_log(4,app,diff_ts)
-	log(loginfo)
-
-def parse_args():
-	global quiet
-	host = ''
-	port = -1
-	helps = 0
-	timeout = 15
-	
-	if(len(sys.argv) == 1):
-		raise Exception("Host and port must set.")
-	
-	offset = 1
-	if ':' in sys.argv[1]:
-		offset = offset + 1
-		hostport = sys.argv[1].split(':')
-		host = hostport[0]
-		port = int(hostport[1])
-	
-	s_params = 'h:p:qt:'
-	l_params = ['host=','port=','timeout=','quiet','help']
-	opts,values= getopt.getopt(sys.argv[offset:],s_params,l_params)
-	
-	for k,v in opts:
-		if k == '--help': helps = 1
-		elif k == '-h' or k == '--host': host = v
-		elif k == '-q' or k == '--quiet': quiet = 1
-		elif k == '-p' or k == '--port': port = int(v)
-		elif k == '-t' or k == '--timeout': timeout = int(v)
-	
-	return host,port,timeout,quiet,helps
-
-		
-def start():
-	try:
-		host,port,timeout,quiet,helps = parse_args()
-		if host=='' or port == -1 or helps == 1:
-			usage()
-			sys.exit(0)
-		wait_for(host,port,timeout)
-	except Exception as err:
-		print(err)
-		usage()
-
-if __name__ == '__main__':
-	start()
-	
+    def wait_for(self, host, port, timeout):
+        self.app = ("%s:%d") %(host,port)
+        sk = socket.socket()
+        logmsg = self.build_log(2, self.app, timeout)
+        if timeout != 0:
+            logmsg = self.build_log(1, self.app, timeout)
+            sk.settimeout(timeout)
+        self.log(logmsg)
+        start_ts = int(time.time())
+        sk.connect((host,port))
+        end_ts = int(time.time())
+        diff_ts= end_ts - start_ts
+        logmsg = self.build_log(3,self.app,diff_ts)
+        self.log(logmsg)
+        
+    def get_parser(self):
+        parser = OptionParser()
+        parser.add_option('-a','--address',dest='address',help='Host or IP under test')
+        parser.add_option('-p','--port',dest='port',help='TCP port under test')
+        parser.add_option('-t','--timeout',dest='timeout',default='15',help='Timeout in seconds, zero for no timeout')
+        parser.add_option('-q','--quiet',dest='quiet',action='store_false',help='Don\'t output any status messages')
+        return parser
+    
+    def verify_options(self):
+        if self.options.address is None:
+            raise OptionException("The address must be set!")
+        elif self.options.port is None:
+            raise OptionException("The port must be set!")
+        elif str(self.options.port).isnumeric() is False:
+            raise OptionException("The value of port must be number!")
+    
+    def start_up(self):
+        try:
+            parser = self.get_parser()
+            self.options,self.args=parser.parse_args()
+            self.verify_options()
+            self.wait_for(self.options.address, int(self.options.port), int(self.options.timeout))
+        except OptionException as err:
+            print(err)
+            parser.print_help()
+        except socket.timeout as err:
+            logmsg = self.build_log(5, self.app, int(self.options.timeout))
+            self.log(logmsg)
+        except ConnectionRefusedError as err:
+            logmsg = self.build_log(4, self.app)
+            self.log(logmsg)
+    
+if __name__=='__main__':
+    w=wait_for_app()
+    w.start_up()
+    
